f0330a6f6ca29aa46012ad2c322b227234b0cb93
sr320 <roberts.sbr@gmail.com>
2019-10-08 14:37:59 -0700

    rprjo

---
+++ R-projects/week_3/scripts/Assignmet_Quiz.R
@@ -0,0 +1,56 @@
+
+
+# This is the R script that I will turn in.....
+
+
+#  EXCERCISE 1 - BASIC EXPESSIONS
+
+
+2 - 10
+3 * 5
+9 / 2
+5 - 3 * 2
+(5 - 3) * 2
+4 ** 2
+8 / 2 ** 2
+
+
+print(2-10)
+
+
+
+
+# EXERCISE #2 
+
+mass_kg <- 2.62
+mass_g <- mass_kg * 1000
+print(mass_g)
+
+
+
+# ex 3
+
+
+
+
+
+
+
+# EXERCISE 4 - BUILT IN FUNCTIONS.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
